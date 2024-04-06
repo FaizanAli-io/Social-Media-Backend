845fd13a8e26cef4bc86c353944e08044bc840e6
Faizan Ali <fali.abdulali@gmail.com>
2024-04-06 16:39:10 +0500

    Major Refractor: Frontend and Backend components seperated into different repositories.

--- FrontEnd/src/views/HomeView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <main>
-    <h1 class="text-emerald-500"> Hello World </h1>
-  </main>
-</template>
--- open.bat
+++
@@ -1,2 +0,0 @@
-start cmd.exe /k "cd backend && python manage.py runserver"
-start cmd.exe /k "code . && cd frontend && npm run dev"
