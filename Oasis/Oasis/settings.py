91e20995407d8ef2ccb07cd0dbad11d325a7dcac
Faizan Ali <fali.abdulali@gmail.com>
2024-04-28 23:27:42 +0500

    Updated settings.py

--- Oasis/Oasis/settings.py
+++ Oasis/Oasis/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".vercel.app", "now.sh"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".vercel.app", ".now.sh"]
 
 # Application definition
 
